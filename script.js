b279aeced2b19526638231aac84490f4559dc403
taskotale <110062804+taskotale@users.noreply.github.com>
2022-11-17 15:49:16 -0500

    added find available function to be able to deterrmin when is last move

--- script.js
+++ script.js
@@ -8,6 +8,18 @@ let gameBoard = [
     ['', '', ''],
 ];
 
+(findAvailableSpots = (board) => {
+    let availableSpots =[];
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (board[i][j] == '') {
+                availableSpots.push(board[i][j])
+            }
+        }
+    }
+    return availableSpots
+})(gameBoard);
+
 (createBoardOnScreen = (whereTo) => {
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
@@ -18,8 +30,6 @@ let gameBoard = [
         }
     }
 })((getIdElementFromDom('board')));
-
-let available = [];
 
 (player = (sign, turn) => {
     let playerTurn = turn;
@@ -32,16 +42,44 @@ let available = [];
 const playerOne = player('X', true);
 const playerTwo = player('O', false);
 
-(gamePlay = (whereToLook, firstPlayer, secondPlayer) => {
-    const grid = whereToLook.querySelectorAll('div');
-    grid.forEach(tile => {
-        tile.addEventListener('click', e => {
-            let playPosition = e.target;
-            playPosition.textContent = checkSign(firstPlayer, secondPlayer);
-            gameBoard[playPosition.id][playPosition.classList] = playPosition.textContent;
-        }, { once: true })
-    })
-})((getIdElementFromDom('board')), playerOne, playerTwo);
+(checkWinner = (board) => {
+    let winner;
+    areEqual = (a, b, c) => {
+        return (a == b && b == c && a != '')
+    }
+    winnerIs = () => {
+        for (let i = 0; i < 3; i++) {
+            if (areEqual(board[i][0], board[i][1], board[i][2])) {
+                winner = board[i][0]
+            }
+        }
+        for (let i = 0; i < 3; i++) {
+            if (areEqual(board[0][i], board[1][i], board[2][i])) {
+                winner = board[0][i]
+            }
+        }
+        if (areEqual(board[0][0], board[1][1], board[2][2])) {
+            winner = board[0][0]
+        }
+
+        if (areEqual(board[0][2], board[1][1], board[2][0])) {
+            winner = board[0][2]
+        }
+        return winner
+    }
+});
+
+    (gamePlay = (whereToLook, firstPlayer, secondPlayer) => {
+        const grid = whereToLook.querySelectorAll('div');
+        grid.forEach(tile => {
+            tile.addEventListener('click', e => {
+                let playPosition = e.target;
+                playPosition.textContent = checkSign(firstPlayer, secondPlayer);
+                gameBoard[playPosition.id][playPosition.classList] = playPosition.textContent;
+                checkWinner(gameBoard)
+            }, { once: true })
+        })
+    })((getIdElementFromDom('board')), playerOne, playerTwo);
 
 (checkSign = (player, otherPlayer) => {
     if (!player.isTurn()) {
@@ -49,5 +87,3 @@ const playerTwo = player('O', false);
     }
     return otherPlayer.sign
 });
-
-
