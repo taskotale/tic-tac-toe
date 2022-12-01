d52ba71da5994c54794d643eb676eb1cfc606481
taskotale <110062804+taskotale@users.noreply.github.com>
2022-12-01 12:54:30 -0500

    cleaning code

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+(getGameboard = () => {
+    return gameBoard = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
+    ];
+});
+
 (player = (sign, turn) => {
     let playerTurn = turn;
     return {
@@ -6,38 +14,37 @@
     }
 });
 
-const playerOne = player('X', true);
-const playerTwo = player('O', false);
+(getPlayerOne = () => {
+    return playerOne = player('X', true)
+});
+(getPlayerTwo = () => {
+    return playerTwo = player('O', false)
+});
 
-(startGame = (...players) => {
-    const selectPlayer = document.createElement('div');
-    selectPlayer.id = 'select-player';
+(getFirstPlayer = (firstPlayer, whereToAppend) => {
     const selectPlayerOne = document.createElement('div');
+    const inputPlayerName = document.createElement('input');
+    selectPlayerOne.appendChild(inputPlayerName);
+    inputPlayerName.addEventListener('keyup', e => { firstPlayer.name = inputPlayerName.value });
+    const firstPlayerSign = document.createElement('div');
+    firstPlayerSign.classList = 'first-player';
+    selectPlayerOne.appendChild(firstPlayerSign);
+    whereToAppend.appendChild(selectPlayerOne);
+});
 
-        const inputPlayerName = document.createElement('input');
-        selectPlayerOne.appendChild(inputPlayerName);
-        inputPlayerName.addEventListener('keyup', e => { players[0].name = inputPlayerName.value });
-        const firstPlayerSign = document.createElement('div');
-        firstPlayerSign.classList = 'first-player';
-        selectPlayerOne.appendChild(firstPlayerSign);
-        
+(getSecondPlayer = (secondPlayer, whereToAppend) => {
     const selectPlayerTwo = document.createElement('div');
     selectPlayerTwo.classList = 'second-player'
-        const playerTwoHuman = document.createElement('div');
-        playerTwoHuman.textContent='HUMAN';
-        playerTwoHuman.addEventListener('click', e=>playerTwoHumanSelection(selectPlayerTwo, playerTwoComputer, playerTwoHuman, players[1]),{ once: true });
-
-        const playerTwoComputer = document.createElement('div');
-        playerTwoComputer.textContent='COMPUTER';
-
-
-        selectPlayerTwo.appendChild(playerTwoHuman);
-        selectPlayerTwo.appendChild(playerTwoComputer);
-
-    selectPlayer.appendChild(selectPlayerOne);
-    selectPlayer.appendChild(selectPlayerTwo);
-    document.querySelector('body').insertBefore(selectPlayer, document.querySelector('body').firstChild);
-})(playerOne, playerTwo);
+    const playerTwoHuman = document.createElement('div');
+    playerTwoHuman.textContent = 'HUMAN';
+    playerTwoHuman.addEventListener('click', e => playerTwoHumanSelection(selectPlayerTwo, playerTwoComputer, playerTwoHuman, secondPlayer), { once: true });
+    const playerTwoComputer = document.createElement('div');
+    playerTwoComputer.textContent = 'COMPUTER';
+    playerTwoComputer.addEventListener('click', e => playerTwoComputerSelection(selectPlayerTwo, playerTwoHuman, playerTwoComputer, secondPlayer), { once: true });
+    selectPlayerTwo.appendChild(playerTwoHuman);
+    selectPlayerTwo.appendChild(playerTwoComputer);
+    whereToAppend.appendChild(selectPlayerTwo);
+});
 
 (playerTwoHumanSelection = (parent, sibling, human, humanName) => {
     parent.removeChild(sibling);
@@ -46,21 +53,37 @@ const playerTwo = player('O', false);
     submitNameBtn.textContent = 'Submit Your Name'
     human.appendChild(playerTwoName);
     human.appendChild(submitNameBtn);
-    submitNameBtn.addEventListener('click', e=> {
+    submitNameBtn.addEventListener('click', e => {
         humanName.name = playerTwoName.value
         parent.parentElement.remove()
     })
 });
 
+(playerTwoComputerSelection = (parent, sibling, computer, computerName) => {
+    parent.removeChild(sibling);
+    const compDumb = document.createElement('div');
+    compDumb.textContent = 'Dumb Computer';
+    const compGenius = document.createElement('div');
+    compGenius.textContent = 'Genius Computer';
+    computer.appendChild(compDumb);
+    computer.appendChild(compGenius);
+    compDumb.addEventListener('click', e => {
+        computerName.name = compDumb.textContent;
+        parent.parentElement.remove()
+    })
+});
+
+(startGame = (...players) => {
+    const selectPlayer = document.createElement('div');
+    selectPlayer.id = 'select-player';
+    getFirstPlayer(players[0], selectPlayer);
+    getSecondPlayer(players[1], selectPlayer);
+    document.querySelector('body').insertBefore(selectPlayer, document.querySelector('body').firstChild);
+})(getPlayerOne(), getPlayerTwo());
+
 (getIdElementFromDom = (id) => {
     return document.getElementById(id)
 });
-
-let gameBoard = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', ''],
-];
 
 (createBoardOnScreen = (whereTo) => {
     for (let i = 0; i < 3; i++) {
@@ -83,11 +106,13 @@ let gameBoard = [
         }
     }
     return availableSpots
-})(gameBoard);
+})(getGameboard());
 
 
 
 (checkTurn = (player, otherPlayer) => {
+    console.log(player)
+    console.log(otherPlayer)
     if (!player.isTurn()) {
         return player.sign
     }
@@ -127,21 +152,23 @@ let gameBoard = [
 });
 
 
-(gamePlay = (whereToLook, firstPlayer, secondPlayer) => {
+(gamePlay = (whereToLook, gameBoard, ...players) => {
     const grid = whereToLook.querySelectorAll('div');
     for (let i = 0; i < grid.length; i++) {
         grid[i].addEventListener('click', e => {
+            console.log(players[0])
+            console.log(players[1])
             let playPosition = e.target;
-            playPosition.textContent = checkTurn(firstPlayer, secondPlayer);
+            playPosition.textContent = checkTurn(players[0], players[1]);
             gameBoard[playPosition.id][playPosition.classList] = playPosition.textContent;
             if (checkWinner(gameBoard) === 'X' || checkWinner(gameBoard) === 'O') {
                 alert('winner is: ' + checkWinner(gameBoard))
-                confirm('do you want to play again?') ? newGame(whereToLook, firstPlayer, secondPlayer) : console.log('no')
+                confirm('do you want to play again?') ? newGame(whereToLook, players[0], players[1]) : console.log('no')
             }
         },
             { once: true })
     }
-})((getIdElementFromDom('board')), playerOne, playerTwo);
+})((getIdElementFromDom('board')), getGameboard(), getPlayerOne(), getPlayerTwo());
 
 
 (newGame = (where, firstPlayer, secondPlayer) => {
